ada2003ca4080438c13e2e7636735a96d7bbb6ca
Michael <anthony_michael_wilson@yahoo.com>
2017-10-22 03:04:12 -0700

    simple update

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 
 var url = "http://st.depositphotos.com/1000315/4169/i/450/depositphotos_41698135-stock-photo-pretty-young-woman-multiple-portrait.jpg";
+
+var offset = 5;
+
 window.onready = function() {
 	$(function() {
 		$.ajax({ 
@@ -21,8 +24,11 @@ window.onready = function() {
 				var box = document.createElement("div");
 				box.title = feeling;				
 				box.className = "box";
-				box.style.left = item["faceRectangle"].left;
-				box.style.top = item["faceRectangle"].top;
+				box.style.left = item["faceRectangle"].left 
+								+ $("#main-photo").offset().left - offset + "px";
+				box.style.top = item["faceRectangle"].top
+								+ $("#main-photo").offset().top - offset + "px";
+				console.log("top: " + box.style.top + "\nleft: " + box.style.left);					
 				box.style.height = item["faceRectangle"].height;
 				box.style.width = item["faceRectangle"].width;
 				$("body").append(box);
@@ -39,8 +45,8 @@ function emotion(person) {
 	var max_emotional_value = 0;
 	for(var key in person) {
 		var current_emotional_value = parseFloat(person[key]);
-		console.log("current emotion: " + key);
-		console.log("current emotional value: " + current_emotional_value);
+		// console.log("current emotion: " + key);
+		// console.log("current emotional value: " + current_emotional_value);
 		if (current_emotional_value > max_emotional_value) {
 			feeling = key;
 			max_emotional_value = current_emotional_value;
