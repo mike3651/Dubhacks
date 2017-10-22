4beb7977599035ac5a08121b30ea5482011adb0f
Brandon Blaschke <brandonj.blaschke@gmail.com>
2017-10-22 03:04:40 -0700

    Capture image and return URL of image
    nowhere near finished

--- website/src/sketch.js
+++ website/src/sketch.js
@@ -10,18 +10,33 @@ let videoWidth;
 let videoHeight;
 
 function setup() {
+
+  //Create canvas
   createCanvas(windowWidth,windowHeight);
   videoHeight = 240;
   videoWidth = 320;
+  background(51);
+
+  //Create webcam
   capture = createCapture(VIDEO);
   capture.size(340,240);
   capture.hide();
+
 }
 
 function draw() {
+
   capture.hide();
   midW = windowWidth / 2;
   midH = windowHeight / 2;
-  background(51);
   image(capture,midW - videoWidth / 2,midH - videoHeight / 2,340,240);
+
+}
+
+function mousePressed() {
+  print("saved image");
+  let temp = get(midW - videoWidth / 2,midH - videoHeight / 2,340,240);
+  image(temp, 50, 50);
+  //temp.save("test", "png");
+  return getURL(temp); 
 }
